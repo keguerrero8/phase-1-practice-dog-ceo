1b3cecc9c1608ef38a1539c90fa56e019f71f095
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-30 12:30:31 -0500

    commit tree 28a17bbe168d9acef60d5023f1cf83a1efcb9ea8

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 console.log('%c HI', 'color: firebrick')
+
+//Challenge 1
+function addImages () {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(data => data.json())
+    .then(json => {
+        //create variable for the div
+        //take one element from json, and append it to the div
+        const div = document.querySelector("#dog-image-container")
+        json.message.forEach((url) => {
+            const img = document.createElement("img")
+            img.src = url
+            div.appendChild(img)
+        }      
+        )
+
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", addImages)
+
+//Challenge 2
+
+function addDogBreeds () {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(data => data.json())
+    .then(json => {
+        const ul = document.querySelector("#dog-breeds")
+        for (let breed in json.message) {
+            const li = document.createElement("li")
+            li.textContent = breed
+            ul.appendChild(li)
+            //li.addEventListener("click", changeColor)
+        }
+    })
+
+}
+addDogBreeds()
+
+//Challenge 3
+function changeColor (event) {
+    //console.log(event.target)
+    event.target.style.color = "blue"
+}
+
+document.addEventListener("click", changeColor)
+
+//Challege 4
+
+
+
+
+
